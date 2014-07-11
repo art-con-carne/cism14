25582be4b0ffbcb50ae6889221de547d1ae92410
Mike Cody <citizencody@gmail.com>
2014-07-10 18:32:47 -0700

    initial model

---
+++ application/models/customers_model.php
@@ -0,0 +1,22 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Customers_model extends CI_Model {
+
+
+	public function __construct()
+	{
+		$this->load->database();
+	}
+	
+	public function get_customers($id=0)
+	{
+		if((int)$id==0)
+		{//id is 0 show all
+			$query = $this->db->get('test_Customers');
+		}else{//show 1 customer
+			$query = $this->db->get_where('test_Customers',array('customerID'=>$id));
+		}
+
+		return $query->result_array();
+	}
+}
